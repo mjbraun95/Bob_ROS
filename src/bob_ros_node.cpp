892ab7cf3b27d3ae7c0f7571ec227830a68b983d
David Lenfesty <lenfesty@ualberta.ca>
2019-01-20 04:02:18 -0700

    Found ar-track-alvar. :(
    Time to move on I guess

--- src/bob_ros_node.cpp
+++ src/bob_ros_node.cpp
@@ -5,32 +5,60 @@
 
 using namespace cv;
 using namespace cv::xfeatures2d;
+using namespace std;
+
+// TODO: switch to FLANN matching
+Ptr<BFMatcher> matcher;
+
+vector<DMatch> find_good_matches(Mat in_descriptors, Mat image) {
+    // List for storage of all matches   
+    vector< vector<DMatch> > matches;
+    vector<DMatch> good_matches;
+
+    // Find matches
+    matcher->knnMatch(in_descriptors, matches, 2);
+
+    /*
+     * matches is a 2d array of matches
+     * the first dimension corresponds to descriptors that had matches.
+     * the second dimension will be the k or less (from knnMatch()) best matches
+     */
+    for (int i = 0; i < static_cast<int>(matches.size()); i++) {
+        // If match is significantly better than next best match
+        if (matches[i][0].distance < matches[i][1].distance * 0.7) {
+            good_matches.push_back(matches[i][0]);
+            cout << "Found match" << endl;
+        }
+    }
+
+    return good_matches;
+}
 
 int main(void) {
     // Detect keypoints and compute descriptors with SURF
     int minHessian = 500;
     Ptr<SURF> detector = SURF::create(minHessian);
     // Match descriptors using FLANN matcher
-    Ptr<BFMatcher> matcher = BFMatcher::create();
+    matcher = BFMatcher::create();
 
     // Detect and compute for image you are trying to find
-    std::vector<KeyPoint> home_keypoints;
-    Mat home_descriptors;
+    vector<KeyPoint> input_keypoints;
+    Mat input_descriptors;
     // Read in checking image
-    Mat home = imread("/home/isthatme/homesymbol_noborder.png", CV_LOAD_IMAGE_COLOR);
-    if (!home.data) {
-        std::cout << "Error reading image" << std::endl;
+    Mat input = imread("/home/isthatme/homesymbol_noborder.png", CV_LOAD_IMAGE_COLOR);
+    if (!input.data) {
+        cout << "Error reading image" << endl;
         return -1;
     }
-    cvtColor(home, home, COLOR_BGR2GRAY); // convert to greyscale
-    detector->detectAndCompute(home, Mat(), home_keypoints, home_descriptors);
-    matcher->add(home_descriptors);
+    cvtColor(input, input, COLOR_BGR2GRAY); // convert to greyscale
+    detector->detectAndCompute(input, Mat(), input_keypoints, input_descriptors);
+    matcher->add(input_descriptors);
 
 
     // Open camera
     VideoCapture cap(0);
     if(!cap.isOpened()) {
-        std::cout << "Failed to open camera!" << std::endl;
+        cout << "Failed to open camera!" << endl;
         return -1;
     }
 
@@ -39,73 +67,62 @@ int main(void) {
 
     // Loop
     for (;;) {
-        Mat input;
-        cap >> input; // Read in from camera
+        Mat frame;
+        cap >> frame; // Read in from camera
 
-        // Detect and compute for image you want to find image in
-        std::vector<KeyPoint> input_keypoints;
-        Mat input_descriptors;
-        Mat input_thing, input_gray;
-        cvtColor(input, input_thing, COLOR_BGR2GRAY); //convert to HSV
-        cvtColor(input, input_gray, COLOR_BGR2GRAY); // convert to greyscale
-        detector->detectAndCompute(input, Mat(), input_keypoints, input_descriptors);
+        Mat frame_gray;
+        cvtColor(frame, frame_gray, COLOR_BGR2GRAY); // convert to greyscale
 
-        // Find matches
-        std::vector< std::vector<DMatch> > matches;
-        matcher->knnMatch(input_descriptors, matches, 2);
+        // Find descriptors for frame image
+        vector<KeyPoint> frame_keypoints;
+        Mat frame_descriptors;
+        detector->detectAndCompute(frame_gray, Mat(), frame_keypoints, frame_descriptors);
+
+        vector<DMatch> good_matches = find_good_matches(frame_descriptors, frame_gray);
+
+
+        // Convert to edges
+        Mat mask;
+        Mat edges;
+        inRange(frame_gray, 127, 255, mask);
+        Canny(frame_gray, edges, 80, 170);
 
         // Find contours
-        Mat mask;
-        std::vector< std::vector<Point> > contours;
-        std::vector<Vec4i> hier;
-        Mat edges;
-        inRange(input_thing, 127, 255, mask);
-        Canny(input_thing, edges, 80, 170);
+        vector< vector<Point> > contours;
+        vector<Vec4i> hier;
         findContours(edges, contours, hier, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
-        Mat contour_img = Mat::zeros(input.rows, input.cols, CV_8UC3);
-
+        // Draw contours
+        Mat contour_img = Mat::zeros(frame.rows, frame.cols, CV_8UC3);
         int idx = 0;
         for (int i = 0; i < static_cast<int>(hier.size()); i++) {
-            if (hier[i][3] >= 0) {
-                drawContours(contour_img, contours, i, Scalar(0, 0, 255));
+            if (contourArea(contours[i]) > 400) {
+                if (hier[i][3] >= 0) {
+                    drawContours(contour_img, contours, i, Scalar(0, 0, 255));
+                }
+                if (hier[i][2] >= 0) {
+                    drawContours(contour_img, contours, i, Scalar(0, 255, 0));
+                }
             }
-            if (hier[i][2] >= 0) {
-                drawContours(contour_img, contours, i, Scalar(0, 255, 0));
-            }
-
         }
 
-        if (contours.size() > 1) {
+        if (contours.size() > 1) { // only show images when there are contours
             imshow("Contours", contour_img);
         }
 
 
-        /*
-         * matches is a 2d array of matches
-         * the first dimension corresponds to descriptors that had matches.
-         * the second dimension will be the k or less (from knnMatch()) best matches
-         */
-
-
-        std::vector<DMatch> good_matches;
-        for (int i = 0; i < static_cast<int>(matches.size()); i++) {
-            if (matches[i][0].distance < matches[i][1].distance * 0.7) {
-                good_matches.push_back(matches[i][0]);
-            }
-        }
-
         // Draw matches
         Mat image1_matches;
+        printf("Matches: %d", static_cast<int>(good_matches.size()));
 
-        if (good_matches.size() >= 10) {
-            drawMatches(input, input_keypoints, home, home_keypoints, good_matches, image1_matches, 
-                    Scalar::all(-1), Scalar::all(-1), std::vector<char>(), 
+        if (good_matches.size() >= 8) {
+            drawMatches(frame, frame_keypoints, input, input_keypoints, good_matches, image1_matches, 
+                    Scalar::all(-1), Scalar::all(-1), vector<char>(), 
                     DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
             imshow("Image_1", image1_matches);
         } else {
-            std::cout << "Not enough Matches!" << std::endl;
+            cout << "Not enough Matches!" << endl;
         }
 
 

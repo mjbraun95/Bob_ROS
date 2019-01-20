dd99e0c3daf270ed7b2740b60358ff7c4b786acc
David Lenfesty <lenfesty@ualberta.ca>
2019-01-20 09:44:26 -0700

    Less shitty square finder

--- src/tile_finder.cpp
+++ src/tile_finder.cpp
@@ -22,7 +22,8 @@ int main(void) {
         Mat edges;
         Mat frame_bw;
         cvtColor(frame, frame_bw, CV_BGR2GRAY);
-        Canny(frame_bw, edges, 100, 230);
+        Scalar m = mean(frame_bw);
+        Canny(frame_bw, edges, m[0]*0.75, m[0]*1.25);
         vector< vector<Point> > contours;
         vector<Vec4i> hier;
         findContours(edges, contours, hier, RETR_TREE, CHAIN_APPROX_SIMPLE);
@@ -31,11 +32,11 @@ int main(void) {
         Mat image = Mat::zeros(frame.rows, frame.cols, CV_8UC3);
 
         for (int i = 0; i < static_cast<int>(contours.size()); i++) {
-            if (contourArea(contours[i]) >= 500) {
+            if (contourArea(contours[i]) >= 300) {
                 vector<Point> poly;
-                approxPolyDP(contours[i], poly, arcLength(contours[i], 0) * 0.12, 0);
+                approxPolyDP(contours[i], poly, arcLength(contours[i], 1) * 0.01, 1);
                 if (poly.size() == 4) {
-                    drawContours(frame, contours, i, Scalar(20, 30, 240), 10);
+                    drawContours(image, contours, i, Scalar(20, 30, 240), 4);
                 }
             }
         }
@@ -43,7 +44,7 @@ int main(void) {
 
 
         if (contours.size() > 0) {
-            imshow("Webcam", frame);
+            imshow("Webcam", image);
         }
 
         imshow("BW", frame_bw);
